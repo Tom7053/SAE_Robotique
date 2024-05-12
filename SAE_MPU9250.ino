6563af04e67b2c5279e095eddddb2c039f9b8f2e
Tom .M <107270908+Tom7053@users.noreply.github.com>
2024-05-12 20:55:36 +0200

    Update SAE_MPU9250.ino

--- SAE_MPU9250.ino
+++ SAE_MPU9250.ino
@@ -1,16 +1,11 @@
-#define F_CPU 16000000UL              // Fréquence du CPU
-#define BAUD 9600                     // Débit en bauds
-#define MYUBRR F_CPU / 16 / BAUD - 1  // Calcul de la valeur d'UBRR
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-
-#include "MPU9250.h"  // Ajout de la bibliothèque pour le capteur MPU9250
+#include "MPU9250.h" // Ajout de la bibliothèque pour le capteur MPU9250
 
 #define PI 3.14159
 
-MPU9250 IMU(Wire,0x68);  // Déclaration de l'objet MPU9250
+MPU9250 IMU(Wire, 0x68); // Déclaration de l'objet MPU9250
 int status;
 
 // Initialisation de l'USART
@@ -33,18 +28,47 @@ void USART_Transmit(unsigned char data) {
   UDR0 = data;
 }
 
+
+// Lecture de la tension du capteur sur la broche spécifiée
+double lireTensionCapteur(uint8_t broche) {
+  // Configuration de la broche en entrée analogique jusqu'à 7
+  ADMUX = (1 << REFS0) | (broche & 00000111);
+
+  // Démarrer la conversion analogique-numérique
+  ADCSRA |= (1 << ADSC) | (1 << ADEN);
+
+  // Lire la valeur convertie
+  uint16_t valeur = ADC;
+
+  // Convertir la valeur lue en tension (entre 0V et Vcc)
+  double tension = (valeur * 5.0) / 1023.0;
+
+  return tension;
+}
+
+
 int main(void) {
-  USART_Init(MYUBRR);  // Initialisation de l'USART
-  sei();               // Activation des interruptions globales
+  #define F_CPU 16000000UL              // Fréquence du CPU
+  #define BAUD 9600                     // Débit en bauds
+  #define MYUBRR F_CPU / 16 / BAUD - 1  // Calcul de la valeur d'UBRR
+
+  DDRC &= ~(1 << PC0); // PC0 en entrée
+  DDRC &= ~(1 << PC1); // PC1 en entrée
+  DDRC &= ~(1 << PC2); // PC2 en entrée
+
+  USART_Init(MYUBRR); // Initialisation de l'USART
+  sei();              // Activation des interruptions globales
 
   // Initialisation du capteur MPU9250
   Wire.begin();
-  while(!Serial) {}
+  while (!Serial)
+    ;
 
   status = IMU.begin();
   if (status < 0) {
     Serial.println("ERREUR");
-    while(1) {}
+    while (1)
+      ;
   }
 
   while (1) {
@@ -54,15 +78,32 @@ int main(void) {
     double y = IMU.getMagY_uT();
     double angle;
 
-    if (x <= 0.1 || y <= 0.1) {
+    if (x <= 0.5 || y <= 0.5) {
       angle = 0;
-    }
-    else {
+    } else {
       angle = atan2(x, y) * (180 / PI);
     }
 
-    // Transmission de l'angle calculé via USART
-    USART_Transmit(angle);
+    // Lecture des tensions des capteurs
+    double tensionCapteur1 = lireTensionCapteur(PC0);
+    double tensionCapteur2 = lireTensionCapteur(PC1);
+    double tensionCapteur3 = lireTensionCapteur(PC2);
+
+    // Logique de contrôle en fonction des tensions des capteurs
+    char direction;
+    if (tensionCapteur2 > 2.5) { // Si le capteur du milieu détecte le signal fort
+      direction = "avancer"; // Avancer tout droit
+    } else if (tensionCapteur1 > 2.5) { // Si le capteur de gauche détecte le signal fort
+      direction = "gauche"; // Tourner à gauche
+    } else if (tensionCapteur3 > 2.5) { // Si le capteur de droite détecte le signal fort
+      direction = "droite"; // Tourner à droite
+    } else {
+      direction = "avancer"; // Par défaut, avancer tout droit
+    }
+
+    // Transmission de l'angle calculé et de la direction via USART
+    USART_Transmit((unsigned char)angle); // Convertir l'angle en unsigned char
+    USART_Transmit(direction); 
 
     // Attente
     _delay_ms(500);
